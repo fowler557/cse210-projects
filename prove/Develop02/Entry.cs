870749c1a0b3a54a7a40c7eb538fa2ef7aaffbe0
Sam Fowler <sam.fowler@asirobots.com>
2024-02-03 20:20:03 -0700

    I did the prove for 2/3/2024 aka develop02

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Entry
+{
+    public string _prompt;
+    public string _response;
+    public DateTime _entryDate;
+
+
+    public void DisplayEntry() 
+    {
+        Console.WriteLine($"{_entryDate} {_prompt} \n> {_response}\n");
+    }
+    
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        int userChoice = 1;
+
+        Console.WriteLine("Welcome to the Journal program");
+
+        while(userChoice != 5)
+        {
+            Console.WriteLine("Please select one of the following choices: ");
+            Console.WriteLine("1. Write ");
+            Console.WriteLine("2. Display ");
+            Console.WriteLine("3. Load ");
+            Console.WriteLine("4. Save ");
+            Console.WriteLine("5. Quit ");
+
+            userChoice = int.Parse(Console.ReadLine());
+            
+            switch (userChoice)
+            {
+                case 1:
+                    PromptGenerator promptGenerator = new PromptGenerator();
+                    string prompt = promptGenerator.GetRandomPrompt();
+                    Console.Write($"\n{prompt}\n> ");
+                    string response = Console.ReadLine();
+
+
+                    //Entry 
+                    Entry entry = new Entry();
+                    entry._prompt = prompt;
+                    entry._response = response;
+                    entry._entryDate = DateTime.Now;
+                
+                    //Journal
+                    journal._entries.Add(entry);
+                    break;
+                case 2:
+                    journal.DisplayEntries();
+                    break;
+                case 3:
+                    journal.LoadEntries();
+                    break;
+                case 4:
+                    journal.SaveEntries();
+                    break;
+                case 5:
+                    Console.WriteLine("Goodbye! Thanks for using the Journal program!");
+                    break;
+                default:
+                    Console.WriteLine("Please enter a number");
+                    break;
+            }
+        
+        }
+
     }
 }
