17b63f631754126918579b43f8da723887885bd3
Sam Fowler <sam.fowler@asirobots.com>
2024-03-06 21:10:42 -0700

    Learning activity plymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        Rectangle rectangle = new Rectangle("red", 4, 5);
+        Circle circle = new Circle("blue", 6);
+        Square square = new Square("green", 3);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+        shapes.Add(square);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
+
+
     }
 }
